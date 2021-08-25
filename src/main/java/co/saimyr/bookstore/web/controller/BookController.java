24f6abd4ac5032ef77ae8f123069ef60e1399a15
Juan Camilo Gil <jcamilo.gil.ies@gmail.com>
2021-08-24 22:20:01 -0500

    refactor: Se definen mejor las funciones del controlador

--- src/main/java/co/saimyr/bookstore/web/controller/BookController.java
+++ src/main/java/co/saimyr/bookstore/web/controller/BookController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import co.saimyr.bookstore.domain.dto.BookstoreDTO;
 import co.saimyr.bookstore.persistence.entity.BookEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import co.saimyr.bookstore.domain.service.BookService;
@@ -18,32 +20,34 @@ public class BookController {
 	private BookService bookService;
 	
 	@GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public List<BookstoreDTO> getAll() {
-		return bookService.getAll();
+	public ResponseEntity<List<BookstoreDTO>> getAll() {
+		return new ResponseEntity<>(bookService.getAll(), HttpStatus.OK);
 	}
 	
 	@GetMapping(value = "/author/{author}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public List<BookstoreDTO> getByAuthor(@PathVariable("author") String writer) {
-		return bookService.getAllByAuthor(writer);
+	public ResponseEntity<List<BookstoreDTO>> getByAuthor(@PathVariable("author") String writer) {
+		return new ResponseEntity<>(bookService.getAllByAuthor(writer), HttpStatus.OK);
 	}
 	
 	@PostMapping(value = "/new", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public BookstoreDTO newBook(@RequestBody BookstoreDTO book) {
-		return bookService.newBook(book);
+	public ResponseEntity<BookstoreDTO> newBook(@RequestBody BookstoreDTO book) {
+		return new ResponseEntity<>(bookService.newBook(book),HttpStatus.CREATED);
 	}
+
 	@GetMapping(value = "/publisher/{publisher}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public List<BookstoreDTO> getByPublisher(@PathVariable("publisher") String publisher){
-		return bookService.findByPublisher(publisher);
+	public ResponseEntity<List<BookstoreDTO>> getByPublisher(@PathVariable("publisher") String publisher){
+		return new ResponseEntity<>(bookService.findByPublisher(publisher), HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/book/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public BookstoreDTO getBook(@PathVariable("id") int id){
-		return bookService.getBook(id);
+	public ResponseEntity<BookstoreDTO> getBook(@PathVariable("id") int id){
+		return new ResponseEntity<>(bookService.getBook(id),HttpStatus.OK);
 	}
 
 	@DeleteMapping(value = "/delete/{id}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE )
-	public void deleteBook(@PathVariable("id") int id ){
+	public ResponseEntity<Void> deleteBook(@PathVariable("id") int id ){
 		bookService.delete(id);
+		return new ResponseEntity<Void>(HttpStatus.ACCEPTED);
 	}
 
 }
